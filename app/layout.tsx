f78b2a1dc2e22e67864d4411938b32216fc0c8e7
Muhammed Fatih Kartcı <muhammed.kartci@gmail.com>
2025-07-25 23:38:28 +0300

    Metadata düzenlendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,20 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Seni Çok Seviyorum ',
   description: 'Mamiş <3 İrmik',
-
-}
+  openGraph: {
+    title: "Seni Çok Seviyorum",
+    description: "Mamiş <3 İrmik",
+    url: "https://irmak.muhammedkartci.com",
+    siteName: "Mamiş <3 İrmik",
+    images: [
+      {
+        url: "https://irmak.muhammedkartci.com/images/mamis-irmik-(3).jpeg", // ✅ Mutlak URL
+        width: 300,
+        height: 1200,
+      },
+    ],
+  }
+};
 
 export default function RootLayout({
   children,
